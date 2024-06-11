6062752a3b491df4897767061b8cf8495808f852
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-06-11 23:50:44 +0100

    Define Fields

--- stock_scraper/stock_scraper/items.py
+++ stock_scraper/stock_scraper/items.py
@@ -7,6 +7,17 @@ import scrapy
 
 
 class StockScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    date = scrapy.Field()
+    current_price = scrapy.Field()
+    volume = scrapy.Field()
+    opening_price = scrapy.Field()
+    closing_price = scrapy.Field()
+    highest_price = scrapy.Field()
+    lowest_price = scrapy.Field()
+    variation = scrapy.Field()
+    volatility = scrapy.Field()
+    capital_exchange = scrapy.Field()
+    market_capitalization = scrapy.Field()
+    isin = scrapy.Field()
+    ticker_symbol = scrapy.Field()
     pass
