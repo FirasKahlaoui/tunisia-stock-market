6fc1fd35c94d4f390568ac188f6b473ef2bf4f3d
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-06-12 15:24:07 +0100

    Fix the path problem

--- stock_scraper/stock_scraper/spiders/stock_spider.py
+++ stock_scraper/stock_scraper/spiders/stock_spider.py
@@ -1,3 +1,8 @@
 import scrapy
 from stock_scraper.items import StockScraperItem
+import json
 
+# Load the JSON file
+file_path = 'companies.json'
+with open(file_path, 'r') as file:
+    data = json.load(file)
