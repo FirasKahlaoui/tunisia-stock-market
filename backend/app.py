9ec13d192816cfbae8618a3572c4846a9ba1b272
YasmineZaatour <117782662+YasmineZaatour@users.noreply.github.com>
2024-06-25 17:49:28 +0100

    Testing the closingPrice prediction

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 CORS(app)
 
 # Load your trained model
-model = pickle.load(open('stock_model.pkl', 'rb'))
+model = pickle.load(open('backend/test_model.pkl', 'rb'))
+scaler_features = pickle.load(open('backend/scaler.pkl', 'rb'))
+target_scaler = pickle.load(open('backend/target_scaler.pkl', 'rb'))
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -20,11 +23,17 @@ def predict():
         data['volume'],
     ]).reshape(1, -1)
     
+    # Scale the input features
+    features_scaled = scaler_features.transform(features)
+
     # Predict the closing price
-    prediction = model.predict(features)
+    prediction_scaled = model.predict(features_scaled)
+    
+    # Unscale the predicted closing price
+    prediction_unscaled = target_scaler.inverse_transform(prediction_scaled.reshape(1,-1))
+
     response = {
-        'closingPrice': prediction[0],
-        'additionalInfo': 'Example additional information'
+        'closingPrice': prediction_unscaled[0][0]
     }
     return jsonify(response)
 
---
+++ backend/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+from flask_cors import CORS
