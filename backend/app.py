25b9d2198c8dec9e03a790ed9a764d15dc907ad9
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-06-26 11:28:14 +0100

    Perdection done

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
+import pandas as pd 
 import pickle
 
 app = Flask(__name__)
@@ -11,26 +12,25 @@ model = pickle.load(open('model.pkl', 'rb'))
 scaler_features = pickle.load(open('scaler.pkl', 'rb'))
 target_scaler = pickle.load(open('target_scaler.pkl', 'rb'))
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    # Extract features from the request data
-    features = np.array([
+    # Convert the input data to a pandas DataFrame with column names
+    features_df = pd.DataFrame([[
         data['openingPrice'],
         data['highestPrice'],
         data['lowestPrice'],
         data['volume'],
-    ]).reshape(1, -1)
+    ]], columns=['openingPrice', 'highestPrice', 'lowestPrice', 'volume'])
     
     # Scale the input features
-    features_scaled = scaler_features.transform(features)
+    features_scaled = scaler_features.transform(features_df)
 
     # Predict the closing price
     prediction_scaled = model.predict(features_scaled)
     
     # Unscale the predicted closing price
-    prediction_unscaled = target_scaler.inverse_transform(prediction_scaled.reshape(1,-1))
+    prediction_unscaled = target_scaler.inverse_transform(prediction_scaled.reshape(-1, 1))
 
     response = {
         'closingPrice': prediction_unscaled[0][0]
